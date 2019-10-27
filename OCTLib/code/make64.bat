a035478daea64f5b120debde9783c23e356df88b
Stepan Baranov <stepan@baranov.us>
2019-10-27 12:51:25 -0400

    set VS160COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\ to allow compile OCTLib x64-bit library with Microsoft Visual Studio 2019 Enterprise edition

--- OCTLib/code/make64.bat
+++ OCTLib/code/make64.bat
@@ -4,8 +4,9 @@
 set MYPROG=OCTLib_64
 set EXCLUDE.1=lin_regr
 set EXCLUDE.2=peak_width
+set VS160COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\
 
-@call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
+@call "%VS160COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 echo.
 
@@ -13,7 +14,6 @@ echo.
 @ for /f "usebackq delims==. tokens=1-3" %%i in (`set EXCLUDE`) do del "../bin/%%k.obj"
 rc /i "../include/" /d "NDEBUG" /d "WIN64" /l 0x409 /fo"../bin/%MYPROG%.res" OCTLib.rc
 link /DLL /NOASSEMBLY /LIBPATH:../lib libfftw3-3-x64.lib /OPT:REF /MACHINE:X64 /NOLOGO /LTCG /OUT:../bin/%MYPROG%.dll ../bin/*.obj ../bin/%MYPROG%.res
-mt -nologo -manifest "../bin/%MYPROG%.dll.manifest" -outputresource:"../bin/%MYPROG%.dll";2
-del "../bin/*.obj" "../bin/%MYPROG%.exp" "../bin/%MYPROG%.res" "../bin/%MYPROG%.dll.manifest"
+del "../bin/*.obj" "../bin/%MYPROG%.exp" "../bin/%MYPROG%.res"
 
 echo %MYPROG% is compiled
